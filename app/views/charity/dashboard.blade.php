f6c32f622c1483e2e65e01744ad3035f64314cb3
Aidan Grabe <aidsgrabe@gmail.com>
2014-02-25 11:08:12 +0000

    Added 'Back to Dashboard' link on charity dashboard
    Made page links clickable on charity dashboard page
    
    Added some lang strings

--- app/views/charity/dashboard.blade.php
+++ app/views/charity/dashboard.blade.php
@@ -1,9 +1,11 @@
 <section>
+    {{ HTML::link('users/dashboard', '&larr; ' . Lang::get('dashboard.back')) }}
+
     <h2>Pages</h2>
     @if (count($pages) > 0)
         <ul>
             @foreach ($pages as $page)
-                <li>{{ $page->title }}</li>
+                <li>{{ HTML::link("c/charity/{$charity->name}/$page->page_id", $page->title) }}</li>
             @endforeach
         </ul>
     @else
