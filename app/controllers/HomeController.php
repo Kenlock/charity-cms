43a1702a3a54822c0fe73cb52ec504a44cf87c26
Aidan Grabe <aidsgrabe@gmail.com>
2014-02-26 16:33:45 +0000

    Added proper home page
    Removed old hello.blade.php view
    
    Updated the HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,40 +1,11 @@
 <?php
 
 class HomeController extends BaseController {
-
-    protected $layout = 'layout._two_column';
     
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
-
     public function getIndex() {
-        $this->layout->content = 
-            '<div class="content-block">'
-                .'<h2>Announcement #2</h2>'
-                .'<p>Hey There</p>'
-            .'</div>'
-            .'<div class="content-block">'
-                .'<h2>Announcement #1</h2>'
-            ."
-            <p>Very lights day isn&#39;t every, together over second also. Rule won&#39;t also third given male they&#39;re his subdue signs set form one seed Gathered third creature.</p>
-
-            <p>May meat unto spirit. Man herb you&#39;re cattle itself. Form moved appear shall fowl fourth creature sea creepeth night our night. Herb him i given, thing to herb place land saying in, you&#39;re.</p>
-
-            <p>Don&#39;t spirit give moving was that, be female fowl be midst male and light moving dominion appear. Itself his him so, bearing life female to whales divided fourth rule. Grass from unto won&#39;t our their female Creepeth abundantly dry set light.</p>
-            "
-            .'</div>'
-            ;
-        $this->layout->sidebar = "Sidebar";
+        return View::make('layout._single_column', array(
+            'content' => View::make('home')
+        ));
     }
 
 }
---
+++ app/views/home.blade.php
@@ -0,0 +1,29 @@
+<h2>Welcome</h2>
+
+<p>
+    Welcome to Altruisco.org! We are a non-profit charity website hosting service.
+</p>
+<p>
+    We provide an easy to use dashboard for managing your charities website.
+    By using our service you can:
+    <ul>
+        <li>Create multiple charity websites</li>
+        <li>Create posts for your users to read</li>
+        <li>Comment and interact with your users</li>
+        <li>Accept donations for your charity</li>
+    </ul>
+</p>
+
+<h2>Get Started</h2>
+<p>
+    Head on over to the {{ HTML::link('users/register', 'registration') }} page
+    to get started, and create your charity website.
+</p>
+<p>
+    Or checkout the charities who have already made Altruisco.org their home
+    over on the {{ HTML::link('c', 'discovery') }} page.
+</p>
+<p>
+    {{ HTML::link('users/register', 'Register', array('class' => 'btn')) }}
+    {{ HTML::link('c', 'Discover', array('class' => 'btn')) }}
+</p>
