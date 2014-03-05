a09bbf56fb11e5e0002e300f910b9c3fce4acc38
Aidan Grabe <aidsgrabe@gmail.com>
2014-03-05 16:15:05 +0000

    Fixed bug where about page wouldn't show page indicator

--- app/views/charity/navbar.blade.php
+++ app/views/charity/navbar.blade.php
@@ -5,9 +5,15 @@
 <ul>
     @if (count($pages) > 0)
         @foreach ($pages as $p)
-            <?php $class = $page->page_id == $p->page_id ? array('class' => 'current') : array(); ?>
+            <?php
+                $class = array();
+                if (isset($page)) {
+                    $class = $page->page_id == $p->page_id ? array('class' => 'current') : array();
+                }
+            ?>
             <li>{{ HTML::link("c/charity/{$charity->name}/{$p->page_id}", $p->title, $class) }}</li>
         @endforeach
-        <li>{{ HTML::link("c/about/{$charity->name}", "About") }}</li>
+        <?php $class = array(); if (!isset($page)) $class = array('class' => 'current'); ?>
+        <li>{{ HTML::link("c/about/{$charity->name}", "About", $class) }}</li>
     @endif
 </ul>
