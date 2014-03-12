d652b61740dc8d233cdec0e6e5e0d4ae77c9a4b1
Aidan Grabe <aidsgrabe@gmail.com>
2014-03-12 18:03:43 +0000

    Updated contact email view

--- app/views/emails/contact.blade.php
+++ app/views/emails/contact.blade.php
@@ -1,12 +1,16 @@
 @extends('emails.master')
 
 @section('body')
+
+    <h2>New Message:</h2>
     {{ isset($name) ? $name . ': ' : '' }}{{ $email }}
     has contacted you via the contact page on the website:
 
-    """
+    <p>
+        """
 
-    {{ $content }}
+        {{ $content }}
 
-    """
+        """
+    <p>
 @overwrite
