bf44d511051394aaed5521624d3fa14b0901fdfa
Aidan Grabe <aidsgrabe@gmail.com>
2014-03-14 00:22:03 +0000

    Fixed javascript error when trying to prevent double click for PayPal form

--- public/js/double_click.js
+++ public/js/double_click.js
@@ -19,14 +19,15 @@
     for (var i = 0; i < forms.length; i++) {
         var form = forms[i];
         var submit = form.querySelector('input[type=submit]');
-        window.addEvent(submit, 'click', function(e) {
-            //e.preventDefault();
-            form.submit();
-            submit.disabled = true;
-            setTimeout(function() {
-                submit.disabled = false;
-            }, delayTime);
-        });
+        if (submit) {
+            window.addEvent(submit, 'click', function(e) {
+                form.submit();
+                submit.disabled = true;
+                setTimeout(function() {
+                    submit.disabled = false;
+                }, delayTime);
+            });
+        }
     }
 
 })();
